65f0e16b03d1b81db7243bc58b5dd796a6f4c5ad
Laurore83 <laurore83@yahoo.fr>
2024-02-22 14:30:06 +0100

    mise en place du local storage

--- src/composants/ModalAddNote.jsx
+++ src/composants/ModalAddNote.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   MDBBtn,
   MDBModal,
@@ -7,13 +8,27 @@ import {
   MDBModalFooter,
   MDBInput,
 } from "mdb-react-ui-kit";
-import { useState } from "react";
 
 export default function ModalAddNote() {
   const [centralModal, setCentralModal] = useState(false);
+  const [titre, setTitre] = useState("");
+  const [description, setDescription] = useState("");
 
   const toggleOpen = () => setCentralModal(!centralModal);
 
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Empêcher la soumission du formulaire par défaut
+
+    // Stockage des valeurs dans localStorage
+    localStorage.setItem("description", description);
+    localStorage.setItem("titre", titre);
+
+    alert("Données sauvegardées localement.");
+
+    // Effacer les champs après la sauvegarde
+    setDescription("");
+    setTitre("");
+  };
   return (
     <>
       <MDBBtn color="dark" onClick={toggleOpen}>
@@ -22,24 +37,45 @@ export default function ModalAddNote() {
       <MDBModal open={centralModal} setOpen={setCentralModal} tabIndex="-1">
         <MDBModalDialog>
           <MDBModalContent>
-            <MDBModalHeader>
-              <MDBInput type="texte" label="Descritpion"></MDBInput>
-              <MDBInput type="texte" label="Note Titre"></MDBInput>
-              <MDBBtn
-                className="btn-close"
-                color="none"
-                onClick={toggleOpen}
-              ></MDBBtn>
-            </MDBModalHeader>
+            <form onSubmit={handleSubmit}>
+              <MDBModalHeader>
+                {/* <MDBInput
+                  wrapperClass="mb-4"
+                  textarea
+                  id="form4Example3"
+                  rows={4}
+                  label="Message"
+                /> */}
+                <MDBInput
+                  type="texte"
+                  id="description"
+                  label="Descritpion"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></MDBInput>
+                <MDBInput
+                  type="texte"
+                  id="titre"
+                  label="Note Titre"
+                  value={titre}
+                  onChange={(e) => setTitre(e.target.value)}
+                ></MDBInput>
+                <MDBBtn
+                  className="btn-close"
+                  color="none"
+                  onClick={toggleOpen}
+                ></MDBBtn>
+              </MDBModalHeader>
 
-            <MDBModalFooter>
-              <MDBBtn color="light" onClick={toggleOpen}>
-                Annuler
-              </MDBBtn>
-              <MDBBtn type="submit" color="dark">
-                Enregistrer la note
-              </MDBBtn>
-            </MDBModalFooter>
+              <MDBModalFooter>
+                <MDBBtn color="light" onClick={toggleOpen}>
+                  Annuler
+                </MDBBtn>
+                <MDBBtn type="submit" color="dark">
+                  Enregistrer la note
+                </MDBBtn>
+              </MDBModalFooter>
+            </form>
           </MDBModalContent>
         </MDBModalDialog>
       </MDBModal>
