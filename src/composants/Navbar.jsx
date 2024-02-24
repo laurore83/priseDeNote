2484460f10d970893ffaad432a87aee8026bc204
Laurore83 <laurore83@yahoo.fr>
2024-02-24 18:52:17 +0100

    menu burger

--- src/composants/Navbar.jsx
+++ src/composants/Navbar.jsx
@@ -1,21 +1,36 @@
 import {
+  MDBCollapse,
   MDBContainer,
+  MDBIcon,
   MDBNavbar,
   MDBNavbarBrand,
   MDBNavbarLink,
+  MDBNavbarNav,
+  MDBNavbarToggler,
 } from "mdb-react-ui-kit";
 import ModalAddNote from "./ModalAddNote";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [openNavSecond, setOpenNavSecond] = useState(false);
   return (
     <>
-      <MDBNavbar light bgColor="light">
+      <MDBNavbar expand="lg" light bgColor="light">
         <MDBContainer fluid>
           <MDBNavbarBrand href="#">NoteMe</MDBNavbarBrand>
-
-          <MDBNavbarLink active aria-current="page" href="#">
-            <ModalAddNote />
-          </MDBNavbarLink>
+          <MDBNavbarToggler
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setOpenNavSecond(!openNavSecond)}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>{" "}
+          <MDBCollapse navbar open={openNavSecond}>
+            <MDBNavbarNav></MDBNavbarNav>
+            <MDBNavbarLink active aria-current="page" href="#">
+              <ModalAddNote />
+            </MDBNavbarLink>
+          </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
     </>
